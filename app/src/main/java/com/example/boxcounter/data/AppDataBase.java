898962923b9eb83d98debeea8bc02ba4ce1624c3
Juanin92 <j.claveria19@com>
2026-02-03 19:12:03 -0300

    feat: implement room db persistence and BoxDao

---
+++ app/src/main/java/com/example/boxcounter/data/AppDataBase.java
@@ -0,0 +1,31 @@
+package com.example.boxcounter.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.boxcounter.model.BoxEntry;
+
+@Database(entities = {BoxEntry.class}, version = 1, exportSchema = false)
+public abstract class AppDataBase extends RoomDatabase {
+
+    public abstract BoxDao boxDao();
+
+    private static volatile AppDataBase INSTANCE;
+
+    public static AppDataBase getDataBase(final Context context){
+        if (INSTANCE == null){
+            synchronized (AppDataBase.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDataBase.class, "box_counter_db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
