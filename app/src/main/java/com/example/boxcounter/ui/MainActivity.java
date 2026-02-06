fdcb9a27765f5dfe86c399603761c76e151048e5
Juanin92 <j.claveria19@com>
2026-02-06 20:41:06 -0300

    feat: implement design to MainActivity and functionality to components

--- app/src/main/java/com/example/boxcounter/ui/MainActivity.java
+++ app/src/main/java/com/example/boxcounter/ui/MainActivity.java
@@ -1,22 +1,47 @@
 package com.example.boxcounter.ui;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.boxcounter.R;
+import com.example.boxcounter.viewModel.TurnViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TurnViewModel viewModel;
+    private TextView tvQuantity;
+    private Button btnPlus, btnMinus, btnFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        tvQuantity = findViewById(R.id.tvQuantity);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus =  findViewById(R.id.btnMinus);
+        btnFinish =  findViewById(R.id.btnFinish);
+
+        viewModel = new ViewModelProvider(this).get(TurnViewModel.class);
+        viewModel.getActiveTurn().observe(this, turn -> {
+            if (turn != null){
+                tvQuantity.setText(String.valueOf(turn.getQuantity()));
+            }
+        });
+
+        btnPlus.setOnClickListener(v -> {viewModel.increment();});
+        btnMinus.setOnClickListener(v -> {viewModel.decrement();});
+        btnFinish.setOnClickListener(v -> {viewModel.finish();});
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
